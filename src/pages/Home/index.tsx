f183804d5c59ecf450d9e36578ec1872b4c0bf1a
Matheus Santos <matheus.gsantos@ibm.com>
2023-12-27 12:32:05 -0300

    fix ENTRETENIMENTO

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ export function Home() {
   const options = [
     { id: 'ARTES', label: 'Artes', color: '#662d91' },
     { id: 'CIENCIAS', label: 'Ciências e Tecnologia', color: '#7CB9E8' },
-    { id: 'ENTRETERIMENTO', label: 'Entretenimento', color: '#4FFFB0' },
+    { id: 'ENTRETENIMENTO', label: 'Entretenimento', color: '#4FFFB0' },
     { id: 'ESPORTES', label: 'Esportes', color: '#FEBE10' },
     { id: 'GEOGRAFIA', label: 'Geografia', color: '#FF7F50' },
     { id: 'HISTORIA', label: 'História', color: '#F9629F' }
